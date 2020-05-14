79d609e4b75a6c06981cb9c92b36c218f435cd51
John Martin Lindseth <john.martin.lindseth@nav.no>
2020-05-14 16:07:04 +0200

    Add database

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,16 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransf
 group = "no.nav.syfo"
 version = "1.0.0"
 
+val flywayVersion = "5.2.4"
+val hikariVersion = "3.3.0"
 val kafkaVersion = "2.0.0"
 val ktorVersion = "1.3.2"
 val logbackVersion = "1.2.3"
 val logstashEncoderVersion = "5.1"
+val postgresVersion = "42.2.5"
 val prometheusVersion = "0.6.0"
 val smCommonVersion = "1.0.22"
+val vaultJavaDriveVersion = "3.1.0"
 
 plugins {
     kotlin("jvm") version "1.3.72"
@@ -32,6 +36,11 @@ dependencies {
 
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("com.zaxxer:HikariCP:$hikariVersion")
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("com.bettercloud:vault-java-driver:$vaultJavaDriveVersion")
 
     implementation("org.apache.kafka:kafka_2.12:$kafkaVersion")
     implementation("no.nav.syfo.sm:syfosm-common-kafka:$smCommonVersion")
