ba6002539955296c3af4484d7142db41fbae4876
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-09-24 15:43:09 +0200

    Refactor consumerProps in kafkaConsumers

--- src/main/kotlin/no/nav/syfo/clients/KafkaConsumers.kt
+++ src/main/kotlin/no/nav/syfo/clients/KafkaConsumers.kt
@@ -7,24 +7,28 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.util.*
 
+fun kafkaConsumerProperties(
+    env: Environment,
+    vaultSecrets: VaultSecrets
+) = Properties().apply {
+    this[ConsumerConfig.GROUP_ID_CONFIG] = "${env.applicationName}-consumer2"
+    this[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+    this[CommonClientConfigs.RETRIES_CONFIG] = "2"
+    this["acks"] = "all"
+    this["security.protocol"] = "SASL_SSL"
+    this["sasl.mechanism"] = "PLAIN"
+    this["schema.registry.url"] = "http://kafka-schema-registry.tpa:8081"
+    this[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
+    this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
+
+    this["sasl.jaas.config"] = "org.apache.kafka.common.security.plain.PlainLoginModule required " +
+        "username=\"${vaultSecrets.serviceuserUsername}\" password=\"${vaultSecrets.serviceuserPassword}\";"
+    this[CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG] = env.kafkaBootstrapServers
+}
+
 class KafkaConsumers(
     env: Environment,
     vaultSecrets: VaultSecrets
 ) {
-    private val properties = Properties().apply {
-        this[ConsumerConfig.GROUP_ID_CONFIG] = "${env.applicationName}-consumer2"
-        this[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
-        this[CommonClientConfigs.RETRIES_CONFIG] = "2"
-        this["acks"] = "all"
-        this["security.protocol"] = "SASL_SSL"
-        this["sasl.mechanism"] = "PLAIN"
-        this["schema.registry.url"] = "http://kafka-schema-registry.tpa:8081"
-        this[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
-        this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringDeserializer"
-
-        this["sasl.jaas.config"] = "org.apache.kafka.common.security.plain.PlainLoginModule required " +
-            "username=\"${vaultSecrets.serviceuserUsername}\" password=\"${vaultSecrets.serviceuserPassword}\";"
-        this[CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG] = env.kafkaBootstrapServers
-    }
-    val kafkaConsumerSmReg = KafkaConsumer<String, String>(properties)
+    val kafkaConsumerSmReg = KafkaConsumer<String, String>(kafkaConsumerProperties(env, vaultSecrets))
 }
