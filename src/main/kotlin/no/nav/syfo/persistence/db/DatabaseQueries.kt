2733fd9adf903e0dacc3465d9dacd4b4309c55ee
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-09-24 17:10:31 +0200

    Move sql-queries into constant vals

--- src/main/kotlin/no/nav/syfo/persistence/db/DatabaseQueries.kt
+++ src/main/kotlin/no/nav/syfo/persistence/db/DatabaseQueries.kt
@@ -6,156 +6,156 @@ import java.sql.Timestamp
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
 
+const val queryCreateSmManuellBehandling =
+    """
+    INSERT INTO smManuellBehandling(
+        sykmelding_id,
+        created,
+        data
+        )
+    VALUES  (?, ?, ?)
+    """
+
 fun DatabaseInterface.createSmManuellBehandling(data: String, sykmeldingId: String) {
     connection.use { connection ->
-        connection.prepareStatement(
-            """
-            INSERT INTO smManuellBehandling(
-                sykmelding_id,
-                created,
-                data
-                )
-            VALUES  (?, ?, ?)
-            """
-        ).use {
+        connection.prepareStatement(queryCreateSmManuellBehandling).use {
             it.setString(1, sykmeldingId)
             it.setTimestamp(2, Timestamp.from(OffsetDateTime.now(ZoneOffset.UTC).toInstant()))
             it.setObject(3, data.toPGObject())
             it.executeUpdate()
         }
-
         connection.commit()
     }
 }
+
+const val queryCreateSmAutomatiskBehandling =
+    """
+    INSERT INTO smAutomatiskBehandling(
+        sykmelding_id,
+        created,
+        data
+        )
+    VALUES  (?, ?, ?)
+    """
 
 fun DatabaseInterface.createSmAutomatiskBehandling(data: String, sykmeldingId: String) {
     connection.use { connection ->
-        connection.prepareStatement(
-            """
-            INSERT INTO smAutomatiskBehandling(
-                sykmelding_id,
-                created,
-                data
-                )
-            VALUES  (?, ?, ?)
-            """
-        ).use {
+        connection.prepareStatement(queryCreateSmAutomatiskBehandling).use {
             it.setString(1, sykmeldingId)
             it.setTimestamp(2, Timestamp.from(OffsetDateTime.now(ZoneOffset.UTC).toInstant()))
             it.setObject(3, data.toPGObject())
             it.executeUpdate()
         }
-
         connection.commit()
     }
 }
+
+const val queryCreateSmHist =
+    """
+    INSERT INTO smHist(
+        sykmelding_id,
+        created,
+        data
+        )
+    VALUES  (?, ?, ?)
+    """
 
 fun DatabaseInterface.createSmHist(data: String, sykmeldingId: String) {
     connection.use { connection ->
-        connection.prepareStatement(
-            """
-            INSERT INTO smHist(
-                sykmelding_id,
-                created,
-                data
-                )
-            VALUES  (?, ?, ?)
-            """
-        ).use {
+        connection.prepareStatement(queryCreateSmHist).use {
             it.setString(1, sykmeldingId)
             it.setTimestamp(2, Timestamp.from(OffsetDateTime.now(ZoneOffset.UTC).toInstant()))
             it.setObject(3, data.toPGObject())
             it.executeUpdate()
         }
-
         connection.commit()
     }
 }
+
+const val queryCreateSmBehandlingsutfall =
+    """
+    INSERT INTO smBehandlingsutfall(
+        sykmelding_id,
+        created,
+        data
+        )
+    VALUES  (?, ?, ?)
+    """
 
 fun DatabaseInterface.createSmBehandlingsutfall(data: String, sykmeldingId: String) {
     connection.use { connection ->
-        connection.prepareStatement(
-            """
-            INSERT INTO smBehandlingsutfall(
-                sykmelding_id,
-                created,
-                data
-                )
-            VALUES  (?, ?, ?)
-            """
-        ).use {
+        connection.prepareStatement(queryCreateSmBehandlingsutfall).use {
             it.setString(1, sykmeldingId)
             it.setTimestamp(2, Timestamp.from(OffsetDateTime.now(ZoneOffset.UTC).toInstant()))
             it.setObject(3, data.toPGObject())
             it.executeUpdate()
         }
-
         connection.commit()
     }
 }
+
+const val queryCreateSmBehandlingsutfallHist =
+    """
+    INSERT INTO smBehandlingsutfallHist(
+        sykmelding_id,
+        created,
+        data
+        )
+    VALUES  (?, ?, ?)
+    """
 
 fun DatabaseInterface.createSmBehandlingsutfallHist(data: String, sykmeldingId: String) {
     connection.use { connection ->
-        connection.prepareStatement(
-            """
-            INSERT INTO smBehandlingsutfallHist(
-                sykmelding_id,
-                created,
-                data
-                )
-            VALUES  (?, ?, ?)
-            """
-        ).use {
+        connection.prepareStatement(queryCreateSmBehandlingsutfallHist).use {
             it.setString(1, sykmeldingId)
             it.setTimestamp(2, Timestamp.from(OffsetDateTime.now(ZoneOffset.UTC).toInstant()))
             it.setObject(3, data.toPGObject())
             it.executeUpdate()
         }
-
         connection.commit()
     }
 }
+
+const val queryCreateSmSykmeldingstatus =
+    """
+    INSERT INTO smSykmeldingstatus(
+        sykmelding_id,
+        created,
+        data
+        )
+    VALUES  (?, ?, ?)
+    """
 
 fun DatabaseInterface.createSmSykmeldingstatus(data: String, sykmeldingId: String) {
     connection.use { connection ->
-        connection.prepareStatement(
-            """
-            INSERT INTO smSykmeldingstatus(
-                sykmelding_id,
-                created,
-                data
-                )
-            VALUES  (?, ?, ?)
-            """
-        ).use {
+        connection.prepareStatement(queryCreateSmSykmeldingstatus).use {
             it.setString(1, sykmeldingId)
             it.setTimestamp(2, Timestamp.from(OffsetDateTime.now(ZoneOffset.UTC).toInstant()))
             it.setObject(3, data.toPGObject())
             it.executeUpdate()
         }
-
         connection.commit()
     }
 }
 
+const val queryCreateSmSykmeldingstatusHist =
+    """
+    INSERT INTO smSykmeldingstatusHist(
+        sykmelding_id,
+        created,
+        data
+        )
+    VALUES  (?, ?, ?)
+    """
+
 fun DatabaseInterface.createSmSykmeldingstatusHist(data: String, sykmeldingId: String) {
     connection.use { connection ->
-        connection.prepareStatement(
-            """
-            INSERT INTO smSykmeldingstatusHist(
-                sykmelding_id,
-                created,
-                data
-                )
-            VALUES  (?, ?, ?)
-            """
-        ).use {
+        connection.prepareStatement(queryCreateSmSykmeldingstatusHist).use {
             it.setString(1, sykmeldingId)
             it.setTimestamp(2, Timestamp.from(OffsetDateTime.now(ZoneOffset.UTC).toInstant()))
             it.setObject(3, data.toPGObject())
             it.executeUpdate()
         }
-
         connection.commit()
     }
 }
