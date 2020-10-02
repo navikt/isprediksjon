cd8d6596b5d2cf771157be11012dc5d2c5e3c202
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-10-02 11:45:48 +0200

    Replace fuel with ktor-cio HttpClient in in AktorregisterClient

--- src/main/kotlin/no/nav/syfo/clients/aktor/AktorService.kt
+++ src/main/kotlin/no/nav/syfo/clients/aktor/AktorService.kt
@@ -6,12 +6,12 @@ import no.nav.syfo.log
 class AktorService(
     private val aktorregisterClient: AktorregisterClient
 ) {
-    fun getFodselsnummerForAktor(aktorId: AktorId, callId: String) =
+    suspend fun getFodselsnummerForAktor(aktorId: AktorId, callId: String) =
         aktorregisterClient.getNorskIdent(aktorId.value, callId).mapLeft {
             throw IllegalStateException("Did not find Aktor")
         }
 
-    fun fodselsnummerForAktor(aktorId: AktorId, callId: String): String? {
+    suspend fun fodselsnummerForAktor(aktorId: AktorId, callId: String): String? {
         var fnr: String? = null
         getFodselsnummerForAktor(aktorId, callId).mapLeft {
             val message = "Did not find Fodelsnummer for AktorId"
