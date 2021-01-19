fb58d32769abcb7ed8fbc825e40b9df1164d00b4
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-19 11:19:46 +0100

    Bump plugin com.github.johnrengelman.shadow
    Bump plugin com.github.johnrengelman.shadow 5.2.0 -> 6.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ object Versions {
 
 plugins {
     kotlin("jvm") version "1.4.10"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 }
 
