4e55a99b6de11879c3b6dd3956dd48c9a6815a99
Mathias Rørvik <mathias.fris.rorvik@nav.no>
2021-02-04 09:37:22 +0100

    back to UTC

--- src/main/resources/db/migration/V5__alter_tables_preiksjon_created.sql
+++ src/main/resources/db/migration/V5__alter_tables_preiksjon_created.sql
@@ -1,8 +1,8 @@
 ALTER TABLE prediksjon_input
-ALTER input_created TYPE timestamptz USING input_created AT TIME ZONE 'CET';
+ALTER input_created TYPE timestamptz USING input_created AT TIME ZONE 'UTC';
 
 ALTER TABLE prediksjon_output
-ALTER input_created TYPE timestamptz USING input_created AT TIME ZONE 'CET';
+ALTER input_created TYPE timestamptz USING input_created AT TIME ZONE 'UTC';
 
 ALTER TABLE prediksjon_output
-ALTER prediksjon_created TYPE timestamptz USING prediksjon_created AT TIME ZONE 'CET';
+ALTER prediksjon_created TYPE timestamptz USING prediksjon_created AT TIME ZONE 'UTC';
