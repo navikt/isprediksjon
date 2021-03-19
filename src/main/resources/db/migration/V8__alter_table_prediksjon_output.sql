4b85a5f439a11570b73fcf8dc3f3eb150f28d7b4
Mathias Rørvik <mathias.fris.rorvik@nav.no>
2021-03-19 10:46:53 +0100

    add default, lint fix

--- src/main/resources/db/migration/V8__alter_table_prediksjon_output.sql
+++ src/main/resources/db/migration/V8__alter_table_prediksjon_output.sql
@@ -1,6 +1,7 @@
 ALTER TABLE prediksjon_output
     ADD COLUMN input_id             integer NOT NULL,
     ADD COLUMN forklaring_raw       jsonb,
-    ADD COLUMN forklaring_front_end jsonb;
+    ADD COLUMN forklaring_front_end jsonb,
+    ALTER COLUMN prediksjon_created SET DEFAULT now();
 
 CREATE INDEX IX_PREDIKSJON_OUTPUT_FNR ON prediksjon_output (fnr);
