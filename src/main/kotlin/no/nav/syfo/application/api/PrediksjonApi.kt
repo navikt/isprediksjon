70654aad7bb16d75919ea31e1260f8834a48c359
John Martin Lindseth <john.martin.lindseth@nav.no>
2021-03-22 15:51:48 +0100

    Add logging

--- src/main/kotlin/no/nav/syfo/application/api/PrediksjonApi.kt
+++ src/main/kotlin/no/nav/syfo/application/api/PrediksjonApi.kt
@@ -8,6 +8,9 @@ import no.nav.syfo.clients.Tilgangskontroll
 import no.nav.syfo.database
 import no.nav.syfo.domain.Fodselsnummer
 import no.nav.syfo.prediksjon.getPrediksjon
+import org.slf4j.LoggerFactory
+
+private val log = LoggerFactory.getLogger("no.nav.syfo.application.api.PrediksjonApiKt")
 
 const val apiBasePath = "/api/v1"
 const val apiPrediksjon = "/prediksjon"
@@ -17,18 +20,24 @@ fun Route.registerPrediksjon(tilgangskontroll: Tilgangskontroll) {
 
     route(apiBasePath) {
         get(apiPrediksjon) {
+            log.info("L-TRACE: GET Prediksjon!")
             val token = call.request.headers[HttpHeaders.Authorization]?.removePrefix("Bearer ")
                 ?: throw IllegalArgumentException("No Authorization header supplied")
+            log.info("L-TRACE: Fikk token!")
 
             val requestFnr =
                 call.request.headers[NAV_PERSONIDENT_HEADER] ?: throw IllegalArgumentException("No Fnr supplied")
+            log.info("L-TRACE: Fikk fnr")
 
             val tilgang = tilgangskontroll.harTilgangTilBruker(Fodselsnummer(requestFnr), token)
 
             if (tilgang) {
+                log.info("L-TRACE: Har tilgang")
                 val pred = database.getPrediksjon(Fodselsnummer(requestFnr))
+                log.info("L-TRACE: Returnerer prediksjon!")
                 call.respond(pred)
             } else {
+                log.info("L-TRACE: Har ikke tilgang!")
                 call.respond(HttpStatusCode.Forbidden)
             }
         }
