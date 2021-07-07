2436964e86275baf9fadc94c637edcd227f2d29f
geir-waagboe <geir.arne.waagbo@nav.no>
2021-07-07 10:52:09 +0200

    Løse manglende update av db-credentials

--- src/main/kotlin/no/nav/syfo/database/VaultCredentialService.kt
+++ src/main/kotlin/no/nav/syfo/database/VaultCredentialService.kt
@@ -7,12 +7,12 @@ import no.nav.syfo.log
 import no.nav.syfo.vault.Vault
 
 class VaultCredentialService {
-    var leaseDuration: Long = 0
+    var leaseDuration: Long = 1
     var renewCredentialsTaskData: RenewCredentialsTaskData? = null
 
     suspend fun runRenewCredentialsTask(applicationState: ApplicationState) {
-        delay(leaseDuration)
-        while (applicationState.ready) {
+        delay(leaseDuration * 1000)
+        while (applicationState.alive) {
             renewCredentialsTaskData?.run {
                 val credentials = getNewCredentials(
                     mountPath,
@@ -27,12 +27,12 @@ class VaultCredentialService {
 
     fun getNewCredentials(mountPath: String, databaseName: String, role: Role): VaultCredentials {
         val path = "$mountPath/creds/$databaseName-$role"
-        log.debug("Getting database credentials for path '$path'")
+        log.info("Getting database credentials for path '$path'")
         try {
             val response = Vault.client.logical().read(path)
             val username = checkNotNull(response.data["username"]) { "Username is not set in response from Vault" }
             val password = checkNotNull(response.data["password"]) { "Password is not set in response from Vault" }
-            log.debug("Got new credentials (username=$username, leaseDuration=${response.leaseDuration})")
+            log.info("Got new credentials (username=$username, role=${role.name}, leaseDuration=${response.leaseDuration})")
             leaseDuration = response.leaseDuration
             return VaultCredentials(response.leaseId, username, password)
         } catch (e: VaultException) {
