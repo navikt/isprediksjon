805c2cd4489547acba0f9f5331c52be7cbb39915
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-08-04 16:33:14 +0200

    Bump Ktlint version from 9.4.1 to 10.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ object Versions {
 plugins {
     kotlin("jvm") version "1.4.21"
     id("com.github.johnrengelman.shadow") version "6.1.0"
-    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
+    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
 }
 
 repositories {
