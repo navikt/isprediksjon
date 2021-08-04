9eb3fdd7602667d290f9ad077bbee7c1c6a2ea77
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-08-04 15:50:53 +0200

    Fix Regex for eleven and thirteen digits

--- src/main/kotlin/no/nav/syfo/domain/AktorId.kt
+++ src/main/kotlin/no/nav/syfo/domain/AktorId.kt
@@ -1,8 +1,10 @@
 package no.nav.syfo.domain
 
-private val thirteenDigits = Regex("\\d{13}")
+data class AktorId(
+    val value: String
+) {
+    private val thirteenDigits = Regex("^\\d{13}\$")
 
-data class AktorId(val value: String) {
     init {
         if (!thirteenDigits.matches(value)) {
             throw IllegalArgumentException("$value is not a valid aktorid")
--- src/main/kotlin/no/nav/syfo/domain/Fodselsnummer.kt
+++ src/main/kotlin/no/nav/syfo/domain/Fodselsnummer.kt
@@ -1,8 +1,10 @@
 package no.nav.syfo.domain
 
-private val elevenDigits = Regex("\\d{11}")
+data class Fodselsnummer(
+    val value: String
+) {
+    private val elevenDigits = Regex("^\\d{11}\$")
 
-data class Fodselsnummer(val value: String) {
     init {
         if (!elevenDigits.matches(value)) {
             throw IllegalArgumentException("$value is not a valid fnr")
