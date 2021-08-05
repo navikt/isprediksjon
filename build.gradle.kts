43fd62163bb75f98699a18e05e147150108164d6
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-08-05 09:17:28 +0200

    Bump Gradle version to 7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ object Versions {
 
 plugins {
     kotlin("jvm") version "1.5.10"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
 }
 
